f2252331e4b4b5b5ddf6c00cd4edebc1f76ce1e3
Merveilles <Merveilleskatumba@gmail.com>
2023-03-17 01:45:38 -0700

    My second C program at ALX

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - using the printf function, display a string of text
- *
- * return: 0
-*/
+ * Return: 0 (success)
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
