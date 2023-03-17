ee88425884a1f160ed52a53d17468f9a1e7ce1bd
Merveilles <Merveilleskatumba@gmail.com>
2023-03-16 19:38:23 -0700

    My first C program at ALX

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
  * main - using puts function, display a string
- *
  * Return: 0
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
