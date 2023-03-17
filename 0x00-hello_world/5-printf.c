aeb3bcee9490c878cc42aa65dd8d89c5be04f0d8
Merveilles <Merveilleskatumba@gmail.com>
2023-03-16 20:32:52 -0700

    My second C program using printf rather than puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - using the printf function, display a string of text
+ *
+ * return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
