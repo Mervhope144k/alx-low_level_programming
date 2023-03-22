df22acccd5a3c8ebed03b0f15da13e873e44274d
Merveilles <Merveilleskatumba@gmail.com>
2023-03-21 17:22:25 -0700

    Nested for loop in action

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 
 	for (n = 1; n <= 10; n++)
 	{
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n');
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
 	}
