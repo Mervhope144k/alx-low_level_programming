91d8397ffb09b8ae74319539bc9a9fbaf47c8af2
Merveilles <Merveilleskatumba@gmail.com>
2023-03-21 17:17:36 -0700

    nested for loop in action

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - display 10 times lowercase alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+	int n = 1;
+	char letter;
+
+	for (n = 1; n <= 10; n++)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+	}
