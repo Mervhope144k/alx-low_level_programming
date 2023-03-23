884bdc6dbbc67f113b8109fb9ed5cb7b0bdb1ad7
Merveilles <Merveilleskatumba@gmail.com>
2023-03-23 10:22:39 -0700

    debugged by changing conditions, identing correctly and properly placing the brackets

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
 * main - prints the largest of 3 integers
 * Return: 0
 */
-
 int main(void)
 {
 	int a, b, c;
@@ -18,6 +16,5 @@ int main(void)
 	largest = largest_number(a, b, c);
 
 	printf("%d is the largest number\n", largest);
-
-	 return (0);
+return (0);
 }
