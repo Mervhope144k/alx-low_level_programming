5cb4a878b114d92c84ffaf7ebe39ae094eb467f0
Merveilles <Merveilleskatumba@gmail.com>
2023-03-23 19:14:59 -0700

    return a * b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - this function multiply two integers when called
+ * @a: first integer
+ * @b: second integer
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
