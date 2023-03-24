a0cb8712efe19377fc7f9152c0f8b071b4d1c759
Merveilles <Merveilleskatumba@gmail.com>
2023-03-23 18:51:59 -0700

    this function is the same as the built-in function isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - this function checks for digits(0 through 1)
+ * @c: is the parameter to check
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
