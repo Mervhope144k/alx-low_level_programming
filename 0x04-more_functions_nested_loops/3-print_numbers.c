16f746aefeb916929955e18df238a2210a7c7eef
Merveilles <Merveilleskatumba@gmail.com>
2023-03-23 19:28:48 -0700

    print char 0 to 9 follow by a newline

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - this function prints 0 to 9 follow by a new line
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
