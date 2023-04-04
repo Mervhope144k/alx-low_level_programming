ca9a8070fd8c3a5de020953f9cb0ccb2b18a05fb
Merveilles <Merveilleskatumba@gmail.com>
2023-04-03 17:18:56 -0700

    advanced pointers, and multi-dimentional arrays

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,20 +5,13 @@
  * @size: argument two,
  * Return: void
  */
-
 void print_diagsums(int *a, int size)
 {
-	int i, j, k, l = 0, r = 0;
+        int i, l = 0, r = 0;
 
-	for (i = 0; i < size; i++)
-	{
-		k = (i * size) + i;
-		l += *(a + k);
-	}
-	for (j = 0; j < size; j++)
-	{
-		k = (j * size) + (size - 1 - j);
-		r += *(a + k);
-	}
-	printf("%i, %i\n", l, r);
+        for (i = 0; i < size * size; i += size + 1)
+                l += a[i];
+        for (i = size - 1; i < size * size - 1; i += size - 1)
+                r += a[i];
+        printf("%d, %d\n", l, r);
 }
