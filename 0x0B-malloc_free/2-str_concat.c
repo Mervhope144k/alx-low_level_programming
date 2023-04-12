a8ed6bd8b5d6dcd6eebb49e2bdc0e34fc5866b4c
Merveilles <Merveilleskatumba@gmail.com>
2023-04-12 13:01:15 -0700

    bug fixed for main_3.c check, segmentation fault also fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,26 +12,18 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, i;
 	char *new_str;
 
-	if (s1 == NULL)
-	{
-		s1 = " ";
-	}
-	if (s2 == NULL)
-	{
-		s2 = " ";
-	}
+	len1 = len2 = 0;
 
-	len1 = 0;
-
-	while (s1[len1] != '\0')
+	if (s1 != NULL)
 	{
+		i = 0;
+		while (s1[i++] != '\0')
 		len1++;
 	}
-
-	len2 = 0;
-
-	while (s2[len2] != '\0')
+	if (s2 != NULL)
 	{
+		i = 0;
+		while (s2[i++] != '\0')
 		len2++;
 	}
 	new_str = (char *) malloc((len1 + len2 + 1) * sizeof(char));
@@ -43,9 +35,11 @@ char *str_concat(char *s1, char *s2)
 	{
 		new_str[i] = s1[i];
 	}
-	for (i = 0; i <= len2; i++)
+	for (i = 0; i < len2; i++)
 	{
 		new_str[len1 + i] = s2[i];
+	new_str[len1 + len2] = '\0';
+
 	}
 	return (new_str);
 }
