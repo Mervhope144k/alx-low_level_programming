f22c622742a9423320f5aecd4dd289fdef7307b1
Merveilles <Merveilleskatumba@gmail.com>
2023-04-12 11:53:00 -0700

    betty requirement fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,7 +5,7 @@
  * create_array - a function that create an array of chars
  * @size: argument of the function
  * @c: argument of the function
- * Return : NULL and 0
+ * Return: NULL and 0
  */
 
 char *create_array(unsigned int size, char c)
