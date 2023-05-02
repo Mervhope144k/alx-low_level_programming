ca60880943b68c5e053ee71343d605e315e442a1
Merveilles Katumba Biende <merveilleskatumba@gmail.com>
2023-05-03 00:23:11 +0200

    bug fixed

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,16 +9,20 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int data;
 
 	if (*head == NULL)
 	{
 		return (0);
 	}
+
 	temp = *head;
+
+	data = (*head)->n
 
 	temp = (*head)->next;
 
 	free(temp);
-	return (n);
+	return (data);
 }
 
