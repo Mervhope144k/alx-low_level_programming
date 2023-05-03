2f47179fefe83718b1e77dad562f16dbf790e755
Merveilles Katumba Biende <merveilleskatumba@gmail.com>
2023-05-03 21:48:45 +0200

    insert new node at given position, bugs on check 10, 7 and 11 fixed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node->n = n;
 
+	if (idx == 0)
+	{
+		new_node->next = curr_node;
+		*head = new_node;
+		return (new_node);
+	}
+
 	for (j = 0; j < (idx - 1); j++)
 	{
 		if (curr_node == NULL || curr_node->next == NULL)
